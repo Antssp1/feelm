14f4a6c6786b78dffb6237aab3203960369f364b
Ayoub Ochan <ayoub.ochan13@gmail.com>
2018-11-14 11:15:45 +0100

    Boutons terminés sans gif pck on est pas assez compétants et qu'on a la flemme

---
+++ src/components/Feel-button/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+
+
+const FeelButton = (props) => (
+    <li className="feel-li">
+        
+        <img src={props.image} className="feel-icone" alt="feel icone"></img>
+        <span className="round-span"></span>
+        <p className="feel-text">{props.text}</p>
+        
+    </li>
+);
+
+export default FeelButton;
