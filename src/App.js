42ed0824f62303f7b3982d575d37a1c23bb31e7e
Audrey M <masambaaudrey1@gmail.com>
2018-11-16 13:02:48 +0100

    Button up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ButtonUp from "./Components/Button-up/index";
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      lastOffset: 0,
+      isVisible: false
+    }
+  }
+
+  handleScroll(evt) {
+    const offset = Math.round(window.pageYOffset)
+
+    if (this.state.lastOffset !== offset) {
+      if (this.state.lastOffset < offset) {
+        this.setState({ isVisible: true, lastOffset: offset })
+      }else{
+        this.setState({ isVisible: false, lastOffset: offset })
+
+      }
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', (evt) => this.handleScroll(evt))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', (evt) => this.handleScroll(evt))
+  }
+
   render() {
     return (
-      <div className="App">
+      <div id="top" className="App" onScroll={(evt) => this.handleScroll(evt)}>
+        <ButtonUp isVisible={this.state.isVisible} />
+        
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Liste films</p>
+          <p>Liste films</p>
         </header>
+        
+
+
+
+
+
+
       </div>
     );
   }
---
+++ src/Components/Button-up/index.js
@@ -0,0 +1,24 @@
+// Button scroll up 
+import React, { Component } from "react";
+import "./index.css";
+import upbutton from './upbutton.png';
+
+
+class ButtonUp extends Component {
+    render() {
+        return (
+            <div className={this.props.isVisible ? 'visible button-up' : 'button-up'}>
+                <a href="../App.js#top" className="buttonlink" >
+                    <img className="button-img" src={upbutton} alt="Button up" />
+                </a>
+            </div>
+        );
+    }
+}
+
+
+
+
+export default ButtonUp
+
+
