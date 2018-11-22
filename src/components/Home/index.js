2516f2dcd1141af122111fb558018bf745d388e0
Audrey M <masambaaudrey1@gmail.com>
2018-11-22 12:11:34 +0100

    modification du css + intégration images background et <p>

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,15 @@
 import React from "react"
 import ListButton from "./components/List-button/index"
+import bkg-home from './bkg-home.gif';
+import Logo from './Logo.png';
 
 const Home = () => {
     return (
-        <div>
+        <div className="container">
+            <img src={Logo} alt="feel'm logo"/>
+            <p>L’application qui selon votre humeur vous propose une liste de film.
+                Avec une base de donnée de plus de 400 000 films.
+            </p>
             <ListButton />
         </div>
     )
