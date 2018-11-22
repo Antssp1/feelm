2f2acb73731cb7c08b7040d07fe263709298568d
Audrey M <masambaaudrey1@gmail.com>
2018-11-22 16:34:46 +0100

    modif du css pour background page Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 
 const Home = () => {
     return (
-        <div className="container">
+        <div className="container-home">
             <img src={Logo} alt="feel'm logo"/>
             <p>L’application qui selon votre humeur vous propose une liste de film.
                 Avec une base de donnée de plus de 400 000 films.
