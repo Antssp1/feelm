98b8d3c5fd4a0b2c93d9684a82548b40f1d7840c
Ayoub Ochan <ayoub.ochan13@gmail.com>
2018-11-23 12:10:18 +0100

    Page HOME terminée (j'espère)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,46 @@
-import React from "react"
+import React, {Component} from "react"
 import Logo from './Logo.png';
 import "./index.css";
 import ListButton from "../../components/List-button/index"
+import Fleche from "./fleche-down.gif"
 
-const Home = () => {
+class Home extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+          lastOffset: 0,
+          isVisible: false
+        }
+      }
+       handleScroll(evt) {
+        const offset = Math.round(window.pageYOffset)
+         if (this.state.lastOffset !== offset) {
+          if (this.state.lastOffset > offset) {
+            this.setState({ isVisible: true, lastOffset: offset })
+          }else{
+            this.setState({ isVisible: false, lastOffset: offset })
+           }
+        }
+      }
+       componentDidMount() {
+        window.addEventListener('scroll', (evt) => this.handleScroll(evt))
+      }
+       componentWillUnmount() {
+        window.removeEventListener('scroll', (evt) => this.handleScroll(evt))
+      }
+       render() {
     return (
-        <div className="container-home">
+        <div className="container-home" onScroll={(evt) => this.handleScroll(evt)}>
             <img src={Logo} alt="feel'm logo"/>
-            <p>L’application qui selon votre humeur vous propose une liste de film.
-                Avec une base de donnée de plus de 400 000 films.
-            </p>
+            <p>Faites le choix qui convient le mieux à votre humeur parmi plus de <b>400 000 films</b> !</p>
+            <div className="div-question-gif">
+            <p className="question">Comment vous sentez-vous ?</p>
+            <img src={Fleche} className={"gif-fleche" + (this.state.isVisible ? "":"-close")} alt="fleche vers le bas"/>
+            </div>
             <ListButton />
         </div>
     )
+}
 }
 
 
