3c8feb47d664ed918c59f4ef26a87f2b93aa792d
Audrey M <masambaaudrey1@gmail.com>
2018-11-24 15:39:26 +0100

    modification du background

---
+++ src/components/Detail/Play-button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import playbutton from "./playbutton.png";
+
+const ButtonPlay = () => (
+    <div>
+        <img src={playbutton}/>    
+    </div>
+)
+
+export default ButtonPlay;
--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React from "react";
+import playbutton from "./playbutton.png";
+import ListeDescription from "./liste-description";
 
 const Detail = () => {
     return (
-        <div>
-           
+        <div className="container-detail">
+            <ListeDescription/>
         </div>
     )
 }
 
 
-export default Detail
+export default Detail;
