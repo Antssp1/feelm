16e65771701f764705fabc2b39f343f8ed458cc6
Audrey M <masambaaudrey1@gmail.com>
2018-11-26 11:07:27 +0100

    ajout components button-Up dans index.js de ACTU

--- src/components/Actu/index.js
+++ src/components/Actu/index.js
@@ -1,16 +1,50 @@
-import React from "react"
-import "./index.css"
-import BoutonTri from "../Bouton-tri/index"
-import Layout from "../scroll/index"
+import React from "react";
+import "./index.css";
+import BoutonTri from "../Bouton-tri/index";
+import Layout from "../scroll/index";
+import ButtonUp from "../Button-up/index";
 
-const Actu = () => {
-    return (
-        <div className="container-actu">
-            <BoutonTri />
-            <Layout />
-        </div>
-    )
+
+class Actu extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            lastOffset: 0,
+            isVisible: false
+        }
+    }
+
+    handleScroll(evt) {
+        const offset = Math.round(window.pageYOffset)
+
+        if (this.state.lastOffset !== offset) {
+            if (this.state.lastOffset < offset) {
+                this.setState({ isVisible: true, lastOffset: offset })
+            } else {
+                this.setState({ isVisible: false, lastOffset: offset })
+            }
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', (evt) => this.handleScroll(evt))
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', (evt) => this.handleScroll(evt))
+    }
+
+    render() {
+        return (
+            <div id="top" className="container-actu" onScroll={(evt) => this.handleScroll(evt)}>
+                <BoutonTri />
+                <Layout />
+                <ButtonUp isVisible={this.state.isVisible} />
+            </div>
+        )
+    }
 }
 
 
-export default Actu
+    export default Actu
