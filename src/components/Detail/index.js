46e1b04942d7f9af9eb3c3630e82619839875d1c
Audrey M <masambaaudrey1@gmail.com>
2018-11-28 15:03:25 +0100

    modification liste description en cours ...

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,10 +1,48 @@
-import React from "react";
-import ListeDescrption from "../liste-description/index"
+import React, {Component} from "react";
+import DetailFilm from "../DetailsFilm/index";
+ 
 
-const Detail =() => (
-    <div>
-        <ListeDescrption/>
-    </div>
-)
+// const Detail =() => (
+//     <div>
+//         <ListeDescrption/>
+//     </div>
+// )
+const idMovie = 338952;
+
+class Detail extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            dataList:[],
+            movieData:[], 
+            cast:[],
+            director:{}
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://api.themoviedb.org/3/movie/338952?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US" )
+        .then(response => response.json())
+        .then (data => {
+            this.setState({
+                dataList : data 
+            })
+        })
+
+    }
+
+
+    render(){
+
+        
+
+        return(
+            <div>
+                <DetailFilm affiche={this.state.dataList.backdrop_path} titre={this.state.dataList.title} annee={this.state.dataList.release_date} description={this.state.dataList.overview} duree={this.state.dataList.runtime} genre={this.state.dataList.genre} ></DetailFilm>
+            </div>
+        )
+    }
+
+}
 
 export default Detail;
--- src/components/DetailsFilm/index.js
+++ src/components/DetailsFilm/index.js
@@ -3,32 +3,34 @@ import "./index.css";
 import ButtonPlay from "./playbutton.png";
 
 
+// <img src={`https://www.youtube.com/watch?v=U0YyQ9TGQa0{props.key}`}  alt="Video" /> 
+
 const DetailsFilm = (props) => {
     return (
         <div className="container-detail">
 
             <div className="header-detail">
-                <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${props.affiche}`}  alt="Affiche film" />
-                
-                <p className="Titre du film">{props.titre}</p>
-                <img src={ButtonPlay} alt="Button Play"/> 
+                  <img src={`https://image.tmdb.org/t/p/w1400_and_h450_bestv2${props.affiche}`}  alt="Affiche film" />
+                    <h2 className="Titre-film">{props.titre}</h2>
+            
             </div>
+           <img src={ButtonPlay} className="button-play" alt="Button Play"/> 
 
             <div className="list-infos-film">
+
                 <ul className="info">
-                    <li className="elemlist">Année : {props.annee} </li>
+                    <li className="right">Année : {props.annee} </li>
                     <li>Genre : {props.genre} </li>
                     <li className="no-bar">Durée : {props.duree} </li>
                 </ul>
                 <ul>
-                    <li>Producteur : {props.producteur} </li>
+                    <li className="right">Producteur : {props.producteur} </li>
                     <li className="no-bar">Acteurs : {props.acteur}  </li>
                 </ul>
                 <p className="description-film">{props.description}</p>
             </div>
             
             <div className="films-similaires">
-                <h3>Film similaires</h3>
             </div>
         </div>
     )
