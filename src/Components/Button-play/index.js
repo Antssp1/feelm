1a363027fb6ab5f30506ec10e2d9337a161c572f
Audrey M <masambaaudrey1@gmail.com>
2018-11-29 17:08:44 +0100

    branch Button-Play - button qui affiche la vidéo avec un fond noir

--- src/Components/Button-play/index.js
+++ src/Components/Button-play/index.js
@@ -6,6 +6,8 @@ import ButtonPlay from "./playbutton.png";
 // remplacer le iframe par le component video
 
 class ButtonsPlayClose extends Component {
+
+
     constructor() {
         super();
         this.state = {
@@ -13,25 +15,25 @@ class ButtonsPlayClose extends Component {
         };
     }
 
-    changeButton() {
-        this.setState((state) => {
-          // Important: read `state` instead of `this.state` when updating.
-          return {ButtonClose: state.count + 1}
-        });
-      }
-    
+ 
 
     render() {
         return (
-            <div>
+            <div className="body">
 
                 <img onClick={() => this.setState({
                     isVisible: !this.state.isVisible
                 })} src={ButtonPlay} className="button-play" alt="Button Play" />
+
+                
+               
                 <br />
                 
                 {this.state.isVisible ? (
                     <div className="video">
+                    <img onClick={() => this.setState({
+                    isVisible: !this.state.isVisible
+                })} src={ButtonClose} className="button-close" alt="Button Play" />
                         <iframe width="560" height="315" src="https://www.youtube.com/watch?v=9_k_goMr5ZI&list=RD9_k_goMr5ZI&start_radio=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 ) : null}
