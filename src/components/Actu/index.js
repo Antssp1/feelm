0726c06424a3f1bd3c40703e14546c4aa3f6bad9
Ayoub Ochan <ayoub.ochan13@gmail.com>
2018-11-30 09:11:07 +0100

    Lien entre bouton-tri et api

--- src/components/Actu/index.js
+++ src/components/Actu/index.js
@@ -1,16 +1,28 @@
-import React from "react"
+import React, {Component} from "react"
 import "./index.css"
 import BoutonTri from "../Bouton-tri/index"
 import Layout from "../scroll/index"
 
-const Actu = () => {
+class Actu extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            apiType : "popularity"
+        }
+    }
+    changeOrder(str){
+        this.setState({
+            apiType: str
+        })
+    }
+    render(){ 
     return (
         <div className="container-actu">
-            <BoutonTri />
-            <Layout />
-           
+            <BoutonTri activate={(str) => this.changeOrder(str)}/>
+            <Layout apiType={this.state.apiType}/>
         </div>
     )
+    }
 }
 
 
