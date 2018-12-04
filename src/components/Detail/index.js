c691282225377e44163f9dcf72b8cda2ceb69779
Audrey M <masambaaudrey1@gmail.com>
2018-12-04 15:13:33 +0100

    modif pour button play

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ButtonsPlayClose from "../Button-play"
 
-// div à rajouter dans DetailFilms
 const Detail = () => {
     return (
         <div className="container-detail">
