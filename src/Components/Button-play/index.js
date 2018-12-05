e47b8a11eea85814fbe63d47100ff15b19740bc1
Audrey M <masambaaudrey1@gmail.com>
2018-12-05 17:27:27 +0100

    supression des commentaire et props displayNon

--- src/Components/Button-play/index.js
+++ src/Components/Button-play/index.js
@@ -42,5 +42,3 @@ class ButtonsPlayClose extends Component {
 }
 
 export default ButtonsPlayClose;
-
-// <iframe width="560" height="315" src={`https://www.youtube.com/embed/${this.props.video}?controls=0`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
